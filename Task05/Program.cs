4410d8cdf8a5fd3391d79f15639482778f8c19bd
Natalya Khlavich <natali.khl@ya.ru>
2023-06-04 23:12:36 +0700

    Добавлены задачи Task00 - Task08

---
+++ Task05/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number < 1)
+    Console.WriteLine("Некорректный ввод");
+else
+{
+    int i = -number;
+    while(i <= number)
+    {
+        Console.WriteLine($"{i} ");
+        ++i;
+    }
+}    
